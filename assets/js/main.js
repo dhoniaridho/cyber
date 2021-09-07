2a59d6f50e25991b1263d8ea074c1dcd36658c7a
dhoniaridho <dhoniaridho@gmail.com>
2021-09-07 20:19:38 +0700

    improve: responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,56 @@
 const todos = [
-    "Love For All, Hatred For None",
-    "Change the world by being yourself.",
-    "Every moment is a fresh beginning.",
-]
+  "Love For All, Hatred For None",
+  "Change the world by being yourself.",
+  "Every moment is a fresh beginning.",
+];
 
 const randomText = [
-    "Love For All, Hatred For None",
-    "Change the world by being yourself.",
-    "Every moment is a fresh beginning.",
-    "Never regret anything that made you smile",
-    "Die with memories, not dreams",
-    "Aspire to inspire before we expire.",
-    "The greatest glory in living lies not in never falling",
-    "The way to get started is to quit talking and begin doing",
-    "Your time is limited, so don't waste it living someone else's life",
-    "If life were predictable it would cease to be life,",
-    "The purpose of our lives is to be happy",
-    "Life is what happens when you're busy making other plans",
-    "Get busy living or get busy dying"
-]
+  "Love For All, Hatred For None",
+  "Change the world by being yourself.",
+  "Every moment is a fresh beginning.",
+  "Never regret anything that made you smile",
+  "Die with memories, not dreams",
+  "Aspire to inspire before we expire.",
+  "The greatest glory in living lies not in",
+  "The way to get started is to quit talking",
+  "Your time is limited, so don't waste it ",
+  "If life were predictable it would cease",
+  "The purpose of our lives is to be happy",
+  "Life is what happens when you're busy",
+  "Get busy living or get busy dying",
+];
 
-document.addEventListener('DOMContentLoaded', ()=>{
+const StrStatus = ["approved", "in progress", "in review", "waiting"];
 
-        var element = '<ul>'
-    
-        todos.forEach(function (todo) {
-            element += '<li class="todo"><label class="check"><input type="checkbox" checked="checked"> <span class="checkmark"></span><p>' + todo + '</p></label></li>'
-        });
-    
-        element += '</ul>';
-        document.getElementById("todos").innerHTML = element
-    
-})
+const Todo = () => {
+  var element = "<ul>";
 
-const addTodos= ()=>{
-    let number = Math.round(Math.random()*10)
-    todos.push(randomText[number])
-    
-    var element = '<ul>'
-    
-    todos.forEach((todo)=> {
-        element += '<li class="todo"><label class="check"><input type="checkbox" checked="checked"> <span class="checkmark"></span><p>' + todo + '</p></label></li>';
-    });
+  todos.forEach(function (todo) {
+    let randomStatus = Math.round(Math.random() * 3);
+    element +=
+      '<li class="todo"><label class="check"><input type="checkbox" checked="checked"> <span class="checkmark"></span><p>' +
+      todo +
+      `</p></label><div class="status-${
+        randomStatus === 0
+          ? "sucess"
+          : randomStatus === 1
+          ? "primary"
+          : randomStatus === 2
+          ? "danger"
+          : randomStatus === 3 ? "secondary" : ''
+      }">${StrStatus[randomStatus]}</div></li>`;
+  });
 
-    element += '</ul>';
-    document.getElementById("todos").innerHTML = element
-}
+  element += "</ul>";
+  document.getElementById("todos").innerHTML = element;
+};
+document.addEventListener("DOMContentLoaded", () => {
+  Todo();
+});
 
+const addTodos = () => {
+  let number = Math.round(Math.random() * 10);
+  todos.push(randomText[number]);
 
+  Todo();
+};
